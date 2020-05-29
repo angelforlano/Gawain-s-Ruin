dea254e9d25d11ec5797894c97274b45480bcb6b
Angel <angel12345leo@gmail.com>
2020-05-29 18:14:28 +0200

    + UpdateAnimator + Move + Jump * Raycaster

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public sealed class Player : MonoBehaviour
 {
+    [Header("Basics Settings")]
     [Range(0, 150)] public int hp = 100;
+    [Range(0.05f, 0.5f)] float turnSmoohtTime = 0.1f;
     [Range(1, 4)] public int walkSpeed = 3;
     [Range(4, 6)] public int runSpeed = 5;
     [Range(4, 6)] public float jumpSpeed = 4;
     [Range(6, 10)] public float gravity = 8;
 
+    [Header("Physics Settings")]
+    public Raycaster groundRaycaster;
+    public Raycaster wallInFrontRaycaster;
+
     int currentSpeed;
+    float turnSmoohtVelocity;
+    Transform mainCamera;
     Animator animator;
-    CharacterController controller;
     Vector3 inputsVector;
     Vector3 moveDirection;
     
@@ -33,62 +40,72 @@ public class Player : MonoBehaviour
 
     public bool IsFalling
     {
-        get {return !controller.isGrounded;}
+        get {return !groundRaycaster.Check();}
     }
     
     void Start()
     {
+        mainCamera = Camera.main.transform;
         animator = gameObject.GetComponent<Animator>();
-        controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        if (!IsAlive)
-            return;
-
-        if (controller.isGrounded)
-        {
-            inputsVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = Camera.main.transform.TransformDirection(inputsVector);
-
-            moveDirection.y = 0;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                //moveDirection.y = jumpSpeed;
-                animator.SetTrigger("jumpToBraced");
-            }
-                
-            // Horizontal Axis Moved or Vertical Axis Moved or 
-            if (Mathf.Abs(inputsVector.x) != 0 || Mathf.Abs(inputsVector.z) != 0)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z)), Time.deltaTime * 10);
-
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    currentSpeed = runSpeed;
-                }
-                else
-                {
-                    currentSpeed = walkSpeed;
-                }
-            } else {
-                currentSpeed = 0;
-            }
-
-            moveDirection = new Vector3(moveDirection.x * currentSpeed, moveDirection.y, moveDirection.z * currentSpeed);
-        }
-
-        moveDirection.y -= gravity * Time.deltaTime;
-        
-        controller.Move(moveDirection * Time.deltaTime);
+        Move();
+        UpdateAnimator();
     }
 
-    void FixedUpdate()
+    void UpdateAnimator()
     {
         animator.SetBool("isRunning", IsRunning);
         animator.SetBool("isWalking", IsWalking);
         animator.SetBool("isFalling", IsFalling);
+    }
+
+    void Move()
+    {
+        if (!IsAlive)
+            return;
+
+        inputsVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+            
+        if (inputsVector.magnitude >= 0.1)
+        {
+            float targetAngle = Mathf.Atan2(inputsVector.x, inputsVector.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoohtVelocity, turnSmoohtTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            if (wallInFrontRaycaster.Check())
+            {
+                currentSpeed = 0;
+            } else if (Input.GetKey(KeyCode.LeftShift)) {
+                currentSpeed = runSpeed;
+            } else {
+                currentSpeed = walkSpeed;
+            }
+
+            if (currentSpeed > 0 )
+            {
+                moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                transform.Translate(moveDirection.normalized * currentSpeed * Time.deltaTime, Space.World);
+            }
+        } else {
+            currentSpeed = 0;
+        }     
+    }
+
+    void Jump()
+    {
+        if (wallInFrontRaycaster.Check())
+        {
+            animator.SetTrigger("jumpToBraced");
+        } else {
+            animator.SetTrigger("jump");
+        }
     }
 }
---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Raycaster : MonoBehaviour
+{
+    public enum RaycastDirection
+    {
+        Forward,
+        Down,
+    }
+
+    public bool debug = true;
+    public RaycastDirection rayDirection;
+    [Range(0.01f, 10)] public float rayLength = 1;
+    public LayerMask targetMask;
+    public string targetTag;
+
+    Vector3 RayDirection
+    {
+        get 
+        {
+            switch (rayDirection)
+            {
+                case RaycastDirection.Forward: return transform.forward;
+                case RaycastDirection.Down: return transform.up * -1;
+                default: return transform.forward;
+            }
+        }
+    }
+
+    public bool Check()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(transform.position, RayDirection, out hitInfo, rayLength, targetMask.value))
+        {
+            if (targetTag != null && targetTag != "")
+            {
+                return hitInfo.collider.gameObject.CompareTag(targetTag);
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!debug) return;
+        
+        if (Check())
+        {
+            Debug.DrawRay(transform.position, RayDirection * rayLength, Color.green);
+        } else {
+            Debug.DrawRay(transform.position, RayDirection * rayLength, Color.red);
+        }
+    }
+}
